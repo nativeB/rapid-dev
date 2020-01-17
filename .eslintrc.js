f54a868b367d16d403f59464031b14e8147b0e12
quincy <qhutchison8@gmail.com>
2020-01-17 04:08:04 +0000

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
-    "extends": "standard",
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
     "rules": {
-        "semi": [
-            "error",
-            "always"
-        ],
-        "camelcase": "error",
-        "comma-dangle": "off",
-        "indent": [
-            "error",
-            2
-        ],
-        "prefer-const": [
-            "error",
-            {
-                "destructuring": "any",
-                "ignoreReadBeforeAssign": false
-            }
-        ]
     }
-}
+};
